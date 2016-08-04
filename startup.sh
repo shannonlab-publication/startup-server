88532941c88a5a24918aa096d9c4b42daaba251f
shannonlab-publication <shannon-contract@googlegroups.com>
2016-08-04 19:59:17 +0900

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+#WorkingDirectory
+WORK_DIR=/usr/local/work
+EXIST_FILE=/var/tmp/.startup
+
+function passwd_root() {
+  echo "管理者パスワードを変更してください。必ずメモしてください。忘れた場合は復旧できません。"
+  passwd
+}
+
+function adduser_develop() {
+  echo "開発者用アカウント(dev)を追加します。パスワードを変更してください。必ずメモしてください。"
+  adduser dev
+  id dev
+  passwd dev
+}
+
+function chef_install() {
+
+  mkdir $WORK_DIR
+  cd $WORK_DIR
+  wget https://packages.chef.io/stable/el/6/chef-12.12.15-1.el6.x86_64.rpm
+  rpm -ivh chef-12.12.15-1.el6.x86_64.rpm
+  RESULT=`rpm -qa | grep chef`
+
+  if [ -n "$RESULT" ]; then
+    echo "CHEF INSTALL SUCCES"
+    mkdir /var/chef
+  fi
+
+}
+
+
+passwd_root
+adduser_develop
+chef_install
